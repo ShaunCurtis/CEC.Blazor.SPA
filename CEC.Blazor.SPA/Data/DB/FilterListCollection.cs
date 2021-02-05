52741c76f061c12c73cc526cd4921a4b6f44d4f4
Shaun Curtis <shaun@coldelm.com>
2021-02-05 22:48:51 +0000

    Evening Commit

---
+++ CEC.Blazor.SPA/Data/DB/FilterListCollection.cs
@@ -0,0 +1,88 @@
+﻿/// =================================
+/// Author: Shaun Curtis, Cold Elm
+/// License: MIT
+/// ==================================
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CEC.Blazor.Data
+{
+
+    public enum FilterListCollectionState
+    {
+        NotSet = 0,
+        Show = 1,
+        Hide = 2
+    }
+
+    public class FilterListCollection :IEnumerable<FilterListItem>
+    {
+        private List<FilterListItem> filterList = new List<FilterListItem>();
+
+        public FilterListCollectionState ShowState { get; set; } = FilterListCollectionState.NotSet;
+
+        public bool OnlyLoadIfFilters { get; set; } = false;
+
+        /// <summary>
+        /// Boolean to determine if the filter should be shown in the UI
+        /// </summary>
+        public bool Show => this.ShowState == FilterListCollectionState.Show;
+
+        /// <summary>
+        /// Boolean to tell the list loader if it need to load
+        /// </summary>
+        public bool Load => this.filterList.Count > 0 || !this.OnlyLoadIfFilters;
+
+        public IEnumerator<FilterListItem> GetEnumerator()
+        {
+            foreach (var item in filterList)
+                yield return item;
+        }
+
+    public bool TryGetFilter(string name, out object value)
+        {
+            value = null;
+            var filter = filterList.FirstOrDefault(FilterListItem => FilterListItem.FieldName.Equals(name));
+            if (filter != default)
+            {
+                value = filter.Value;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SetFilter(string name, object value, bool overwite = true)
+        {
+            var filter = filterList.FirstOrDefault(FilterListItem => FilterListItem.FieldName.Equals(name));
+            if (filter == default)
+            {
+                filterList.Add(new FilterListItem() { FieldName = name, Value = value, ObjectType = value.GetType()});
+                return true;
+            }
+            else
+            {
+                if (overwite)
+                {
+                    filter.Value = value;
+                    filter.ObjectType = value.GetType();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool ClearFilter(string name)
+        {
+            var filter = filterList.FirstOrDefault(FilterListItem => FilterListItem.FieldName.Equals(name));
+            if (filter != default)
+            {
+                filterList.Remove(filter);
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ CEC.Blazor.SPA/Data/DB/FilterListItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEC.Blazor.Data
+{
+    public class FilterListItem
+    {
+        public string FieldName { get; set; }
+
+        public object Value
+        {
+            get => _Value;
+            set
+            {
+                _Value = value;
+            }
+        }
+
+        private object _Value = null;
+
+        public Type ObjectType { get; set; }
+
+    }
+}
